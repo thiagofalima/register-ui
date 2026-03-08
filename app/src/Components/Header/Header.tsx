eb8d850f235a719fd9ea0eb371a52716004cfe00
thiagofalima <thiagofalima@hotmail.com>
2026-03-08 18:20:26 -0300

    feat: add Header component - also add declarations for webp files recognition

---
+++ app/src/Components/Header/Header.tsx
@@ -0,0 +1,33 @@
+import { Button } from "../Button/Button";
+import {
+  BuscarInputContainer,
+  WrapperContainer,
+  Input,
+  Menu,
+  MenuRight,
+  Row,
+  HeaderWrapper,
+} from "./HeaderWrapper";
+import logo from "../../assets/logo-full.webp"
+
+export const Header = () => {
+  return (
+    <WrapperContainer>
+      <HeaderWrapper>
+        <Row>
+          <img src={logo} alt="logo da DIO" />
+          <BuscarInputContainer>
+            <Input placeholder="Buscar..."/>
+          </BuscarInputContainer>
+          <Menu>Live Code</Menu>
+          <Menu>Global</Menu>
+        </Row>
+        <Row>
+          <MenuRight href="#">Home</MenuRight>
+          <Button title="Entrar" />
+          <Button title="Cadastrar" />
+        </Row>
+      </HeaderWrapper>
+    </WrapperContainer>
+  );
+};
--- app/src/Pages/Home.tsx
+++ app/src/Pages/Home.tsx
@@ -1,12 +1,17 @@
 import { Link } from "react-router-dom";
 import { Button } from "../Components/Button/Button";
+import { Header } from "../Components/Header/Header";
 
 export const Home = () => {
   return (
     <>
+      <Header />
       <h1>Home</h1>
       <Button title="Teste" onClick={() => alert("Teste")} />
-      <Button title="Teste" onClick={() => alert("Teste")} variant="secondary"/>
+      <Button
+        title="Teste"
+        variant="secondary"
+      />
       <Link to="/login">Para ir para a tela de login</Link>
     </>
   );
