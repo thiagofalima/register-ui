2200bc070566e51592c007d031839391a4faba78
thiagofalima <thiagofalima@hotmail.com>
2026-03-08 14:18:24 -0300

    style(button): fix border

--- app/src/Components/Button/ButtonWrapper.tsx
+++ app/src/Components/Button/ButtonWrapper.tsx
@@ -1,34 +1,36 @@
 import styled, { css } from "styled-components";
 
-interface IButtonWrapper{
-    variant?: "primary" | "secondary"  // variant? -> Se quiser passar ou não essa prop quando usar o componente
+interface IButtonWrapper {
+  variant?: "primary" | "secondary"; // variant? -> Se quiser passar ou não essa prop quando usar o componente
 }
 
 export const ButtonWrapper = styled.button<IButtonWrapper>`
-  min-width: 120px;
-  width: 100%;
-  padding: 2px 12px;
-
   background: #565656;
   border-radius: 22px;
   position: relative;
-  color: #ffffff;
 
-  ${(props) => 
+  color: #ffffff;
+  padding: 2px 12px;
+  min-width: 120px;
+  width: 100%;
+
+  border: 1px solid #565656;
+
+  ${(props) =>
     props.variant !== "primary" &&
     css`
       min-width: 167px;
       height: 33px;
 
-      background: #E4105B;
+      background: #e4105b;
 
       &::after {
         content: "";
         position: absolute;
-        border: 1px solid #E4105B;
+        border-color: #e4105b;
         top: -5px;
         left: -6px;
-        width: calc(100% - 10px);
+        width: calc(100% + 10px);
         height: calc(100% + 10px);
         border-radius: 22px;
       }
