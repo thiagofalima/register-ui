f7aa9ebcf128222704e44ee7ba9a2eaf8dcc988d
thiagofalima <thiagofalima@hotmail.com>
2026-03-08 12:34:09 -0300

    feat: add Login component and route

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { Home } from './Components/Home';
+import { Login } from './Components/Login';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={ <Home /> }/>
+        <Route path="/login" element={ <Login /> }/>
       </Routes>
     </BrowserRouter>
   );
