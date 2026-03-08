29b919b81e415fcf9253399e30eabf1336256318
thiagofalima <thiagofalima@hotmail.com>
2026-03-08 12:32:49 -0300

    feat: add BrowserRouter - Also Home component - Also add a Home Route

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { Home } from './Components/Home';
 
 function App() {
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={ <Home /> }/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
