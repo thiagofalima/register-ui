2ad8b567c65a69b512b6a124fc75175931938b68
thiagofalima <thiagofalima@hotmail.com>
2026-03-08 12:48:31 -0300

    refactor: migrate Home and Login to pages folder

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import { Home } from './Components/Home';
-import { Login } from './Components/Login';
+import { Home } from './Pages/Home';
+import { Login } from './Pages/Login';
 
 function App() {
   return (
