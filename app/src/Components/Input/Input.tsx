c049729342136a3ae147cf3c5c11c285d8871790
thiagofalima <thiagofalima@hotmail.com>
2026-03-13 14:59:07 -0300

    feat: add react hook forms Controller

--- app/src/Components/Input/Input.tsx
+++ app/src/Components/Input/Input.tsx
@@ -1,39 +1,52 @@
 import { InputHTMLAttributes, ReactNode } from "react";
-import styled from "styled-components"
+import styled from "styled-components";
+import { useForm, Controller, SubmitHandler } from "react-hook-form";
 
 export const InputContainer = styled.div`
-    max-width: 275px;
-    width: 100%;
-    height: 30px;
-    border-bottom: 1px solid #3B3450;
+  max-width: 275px;
+  width: 100%;
+  height: 30px;
+  border-bottom: 1px solid #3b3450;
 
-    display: flex;
-    align-items: center;
-    margin-bottom: 20px;
-`
+  display: flex;
+  align-items: center;
+  margin-bottom: 20px;
+`;
 
 export const IconContainer = styled.div`
-    margin-right: 10px;
-`
+  margin-right: 10px;
+`;
 
 export const InputText = styled.input`
-    background-color: transparent;
-    color: #FFFFFF;
-    border: 0;
-    height: 30px;
-`
+  background-color: transparent;
+  color: #ffffff;
+  border: 0;
+  height: 30px;
+`;
 
 // Herdando dos atributos padrão do input para o React
-interface InputProps extends InputHTMLAttributes<HTMLInputElement>{
-    leftIcon?: any; // tipagem correta para algo que pode ser renderizado
-    name: string;
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  leftIcon?: any; // tipagem correta para algo que pode ser renderizado
+  control?: any;
+  name: string;
 }
 
-export const Input = ({leftIcon, name, ...rest}:InputProps) => {
-    return (
-        <InputContainer>
-        { leftIcon ? (<IconContainer><img src={leftIcon} /></IconContainer>): null }
-            <InputText name={name} {...rest}/>
-        </InputContainer>
-    )
-}
+export const Input = ({ leftIcon, control,  name, ...rest }: InputProps) => {
+  return (
+    <>
+      <Controller
+        name={name}
+        control={control}
+        rules={{required: true}}
+        render={({ field }) => <InputText {...field} {...rest} />}
+      />
+      <InputContainer>
+        {leftIcon ? (
+          <IconContainer>
+            <img src={leftIcon} />
+          </IconContainer>
+        ) : null}
+      </InputContainer>
+    </>
+  );
+};
