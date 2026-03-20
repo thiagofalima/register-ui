fd69aef8abfb96b893e11d31380dde1f50a26fc9
thiagofalima <thiagofalima@hotmail.com>
2026-03-20 16:36:36 -0300

    feat: add useState hook to manage user state

--- app/src/context/auth.tsx
+++ app/src/context/auth.tsx
@@ -1,10 +1,12 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import { IAuthContext, IAuthContextProviderProps } from "./types";
+import { IUser } from "../types/user";
 
 export const AuthContext = createContext<IAuthContext>({} as IAuthContext);
 
-export const AuthContextProvider = ({children}: IAuthContextProviderProps) => {
-    <AuthContext.Provider value={{}}>
-        {children}
-    </AuthContext.Provider>
-}
+export const AuthContextProvider = ({
+  children,
+}: IAuthContextProviderProps) => {
+  const [user, setUser] = useState<IUser>({} as IUser);
+  return <AuthContext.Provider value={{user}}>{children}</AuthContext.Provider>;
+};
