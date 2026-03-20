07a8bb7cba7754a98db784efb06ed9afc28e02eb
thiagofalima <thiagofalima@hotmail.com>
2026-03-20 16:37:58 -0300

    feat: add AuthContextProvider to App

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,19 +1,22 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import { Home } from './Pages/Home';
-import { Login } from './Pages/Login';
-import { Feed } from './Pages/Feed'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home } from "./Pages/Home";
+import { Login } from "./Pages/Login";
+import { Feed } from "./Pages/Feed";
 import { Register } from "./Pages/Register";
+import { AuthContextProvider } from "./context/auth";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={ <Home /> }/>
-        <Route path="/login" element={ <Login /> }/>
-        <Route path="/feed" element={ <Feed /> }/>
-        <Route path="/register" element={ <Register /> }/>
-      </Routes>
-    </BrowserRouter>
+    <AuthContextProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/feed" element={<Feed />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthContextProvider>
   );
 }
 
